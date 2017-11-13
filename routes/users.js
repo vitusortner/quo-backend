cc02f7ebab8fd320b2e3258ce593864320174bf5
Koreuber, Nora <s64392@beuth-hochschule.de>
2017-11-13 15:39:05 +0100

    added some code to user routes

--- routes/users.js
+++ routes/users.js
@@ -5,3 +5,28 @@
  * @module routes/users
  * @type {Router}
  */
+
+// remember: in modules you have 3 variables given by CommonJS
+// 1.) require() function
+// 2.) module.exports
+// 3.) exports (which is module.exports)
+
+// modules
+var express = require('express');
+var logger = require('debug')('we2:pins');
+var codes = require('../restapi/http-codes');
+var HttpError = require('../restapi/http-error.js');
+
+var mongoose = require('mongoose'); // object represents mongoose framework, possible to use all methods of mongoose
+var userModel = require('../models/user'); //pin.js (Schema) saved as pinModel
+
+var users = express.Router();
+
+users.route('/')
+    .all(function(req, res, next) {
+        var err = new HttpError('this method is not allowed at ' + req.originalUrl, codes.wrongmethod);
+        next(err);
+    });
+
+
+module.exports = users;
