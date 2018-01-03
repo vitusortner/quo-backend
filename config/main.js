2e64b82fae5cf314f4fed0dd97ef0fd24e2285a9
Florian Schlüter <s64212@beuth-hochschule.de>
2018-01-03 18:16:45 +0100

    No more / in URL, and s3 url in config file.

--- config/main.js
+++ config/main.js
@@ -3,6 +3,8 @@ module.exports = {
     'secret': '-lfkgnl-nrkwnnlnqlnjfdsjfxmcfsmicvwoc,rwp123lb chvgvh4568fdssdoco33okd njaj',
     // Database connection information
     'database': 'mongodb://localhost:27017/quo',
+    // AWS picture bucket URL
+    'pictureBucket': 'https://s3.eu-central-1.amazonaws.com/quo-picture-bucket/',
     // Setting port for server
     'port': process.env.PORT || 3000
 }
